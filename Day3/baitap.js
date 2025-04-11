b557bcf003d7d10755c6bb4c058fe481d3edcbad
HuyNM139 <huy668217@gmail.com>
2025-04-11 21:09:29 +0700

    update

---
+++ Day3/baitap.js
@@ -0,0 +1,28 @@
+//b1
+//let input1 = prompt("Enter số a:");
+//let input2 = prompt("Enter số b:");
+//let ab = a*b
+//console.log(ab)
+
+//b2
+//let input = prompt("Nhập a:")
+//let output = a * 2
+//console.log(output)
+
+//b3
+//let a = prompt("Nhập a:")
+//let b = prompt("Nhập b:")
+//let c = prompt("Nhập c:")
+//let output = (a - b) * c
+//console.log(output)
+//b4
+//let a = prompt("Nhập a:")
+//let b = prompt("Nhập b:")
+//let output=a+b+a*b
+//console.log(output
+//b5
+//let a = prompt("Nhập a:")
+//let b = prompt("Nhập b:")
+//let c = prompt("Nhập c:")
+//let p = (a+b+c)/2
+//let output = Math.sqrt()
---
+++ Day3/day.js
@@ -0,0 +1,20 @@
+console.log("hello world");
+console.log(typeof"hello world");
+console.log('"123!@%$$*@$@"');
+console.log("'abc'");
+console.log('ab');
+console.log(123);
+console.log(typeof -10);
+console.log("a" + "b");
+console.log("My age is:" + 123);
+console.log(true);
+console.log(false);
+let address = "555b Đại lộ Bình Dương, Hiệp Thành, Thủ Dầu Một, Bình Dương";
+address ="New York"
+address="Bình DƯơng"
+console.log(address);
+console.log(`toi o ${address}.Nha toi dep`);
+let a= 10
+let b = 2
+let count = a*b
+console.log("count = "+count)
